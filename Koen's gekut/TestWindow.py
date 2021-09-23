5b5f5a0c1f469b0b262b495f64fa9bf162f230f9
ReboundQ3 <ReboundQ3@gmail.com>
2021-09-23 13:18:43 +0200

    KoenKoekjeV6

--- Koen's gekut/TestWindow.py
+++ Koen's gekut/TestWindow.py
@@ -1,14 +1,26 @@
 import tkinter as tk
 import webbrowser
+import requests
+import os
 
 
 def laptop():
     webbrowser.open("http://www.themostamazingwebsiteontheinternet.com/")
+    doc = requests.get('http://www.themostamazingwebsiteontheinternet.com/justcantgetenough.mp3')
+    with open('Koens muziekje.mp3', 'wb') as f:
+        f.write(doc.content)
+    os.startfile('Koens muziekje.mp3')
 
 window = tk.Tk()
+
+window.configure(
+    bg="black",
+)
+
+
 label = tk.Label(
-    text="Python rocks!",
-    fg="blue",
+    text="Koenkoekje V6",
+    fg="White",
     bg="black",
 )
 label.pack()
@@ -17,8 +29,8 @@ button = tk.Button(
     text="Klik",
     width=50,
     height=10,
-    bg="pink",
-    fg="purple",
+    bg="black",
+    fg="white",
     command=laptop
 )
 
